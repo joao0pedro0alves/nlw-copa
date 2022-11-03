13f346778da71b8055f77b965fa0ee2b9730c25d
João Alves <joao.alves@ecosis.com.br>
2022-11-03 11:39:28 -0300

    Create app mobile components

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -1,6 +1,6 @@
 import {Button as ButtonNativeBase, IButtonProps, Text} from 'native-base'
 
-interface ButtonProps extends IButtonProps{
+interface ButtonProps extends IButtonProps {
     title: string
     type?: 'PRIMARY' | 'SECONDARY'
 }
@@ -8,21 +8,20 @@ interface ButtonProps extends IButtonProps{
 export function Button({title, type = 'PRIMARY', ...props}: ButtonProps) {
     return (
         <ButtonNativeBase
-            w='full'
+            w="full"
             h={14}
-            rounded='sm'
-            fontSize='md'
+            rounded="sm"
+            fontSize="md"
             bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
             _pressed={{
-                bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600'
+                bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600',
             }}
-
             {...props}
         >
             <Text
-                fontSize='sm'
-                fontFamily='heading'
-                textTransform='uppercase'
+                fontSize="sm"
+                fontFamily="heading"
+                textTransform="uppercase"
                 color={type === 'SECONDARY' ? 'white' : 'black'}
             >
                 {title}
