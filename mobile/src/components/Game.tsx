cfd89356df38e9fbc7cf35a291c7d17a7688fa39
João Alves <joao.alves@ecosis.com.br>
2022-11-04 12:01:10 -0300

    Format game date with dayjs

--- mobile/src/components/Game.tsx
+++ mobile/src/components/Game.tsx
@@ -1,6 +1,8 @@
 import {Button, HStack, Text, useTheme, VStack} from 'native-base'
 import {X, Check} from 'phosphor-react-native'
 import {getName} from 'country-list'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
 
 import {Team} from './Team'
 
@@ -15,6 +17,7 @@ interface GuessProps {
 
 export interface GameProps {
     id: string
+    date: string
     firstTeamCountryCode: string
     secondTeamCountryCode: string
     guess: null | GuessProps
@@ -22,7 +25,7 @@ export interface GameProps {
 
 interface Props {
     data: GameProps
-    onGuessConfirm: () => void
+    onGuessConfirm: (gameId: string) => Promise<void>
     setFirstTeamPoints: (value: string) => void
     setSecondTeamPoints: (value: string) => void
 }
@@ -34,6 +37,8 @@ export function Game({
     onGuessConfirm,
 }: Props) {
     const {colors, sizes} = useTheme()
+
+    const when = dayjs(data.date).locale(ptBR).format("DD [de] MMMM [de] YYYY [às] H:00[h]")
 
     return (
         <VStack
@@ -52,7 +57,7 @@ export function Game({
             </Text>
 
             <Text color="gray.200" fontSize="xs">
-                22 de Novembro de 2022 às 16:00h
+                {when}
             </Text>
 
             <HStack
@@ -82,7 +87,7 @@ export function Game({
                     w="full"
                     bgColor="green.500"
                     mt={4}
-                    onPress={onGuessConfirm}
+                    onPress={() => onGuessConfirm(data.id)}
                 >
                     <HStack alignItems="center">
                         <Text
