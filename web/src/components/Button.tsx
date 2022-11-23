6204955de0baee5a1ea2e54ff38f52b8d7945fc3
João Alves <joao.alves@ecosis.com.br>
2022-11-23 13:37:34 -0300

    Feat: add button loading spinner

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,11 +1,29 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from 'react'
+import { Oval } from 'react-loader-spinner'
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    isLoading?: boolean
+}
 
-export function Button(props: ButtonProps) {
+export function Button({ isLoading, ...props }: ButtonProps) {
     return (
-        <button className="px-6 py-4 rounded bg-yellow-500 text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700">
-            {props.children}
+        <button className="relative h-12 px-6 py-4 rounded bg-yellow-500 text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700">
+            {!isLoading ? (
+                <span>{props.children}</span>
+            ) : (
+                <Oval
+                    height={24}
+                    width={24}
+                    wrapperStyle={{}}
+                    wrapperClass="absolute inset-0 flex items-center justify-center"
+                    visible={true}
+                    ariaLabel="oval-loading"
+                    strokeWidth={3}
+                    strokeWidthSecondary={3}
+                    color='#121214'
+                    secondaryColor='#202024'
+                />
+            )}
         </button>
     )
 }
