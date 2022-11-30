84abbc1e51bd4cf74cc6ebb428d3d7d6fe793980
João Alves <joao.alves@ecosis.com.br>
2022-11-30 14:20:03 -0300

    Feat: auth routes create Jwt token refactoring

--- server/src/plugins/authenticate.ts
+++ server/src/plugins/authenticate.ts
@@ -1,5 +1,20 @@
-import { FastifyRequest } from 'fastify'
+import { User } from '@prisma/client'
+import { FastifyInstance, FastifyRequest } from 'fastify'
 
 export async function authenticate(request: FastifyRequest) {
     await request.jwtVerify()
+}
+
+export function createJwtToken(fastify: FastifyInstance, user: User) {
+    return fastify.jwt.sign(
+        {
+            name: user.name,
+            avatarUrl: user.avatarUrl,
+            isAdmin: user.isAdmin
+        },
+        {
+            sub: user.id, // Quem gerou o token
+            expiresIn: '7 days',
+        }
+    )
 }
